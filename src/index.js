6bea95b20aac6031d675dd46abfd8e5483d9964c
Leonardo Ribeiro <leoribeiro.dsgn@gmail.com>
2019-10-04 18:01:55 -0300

    removed sequelize connection temporary

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
-import Sequelize from 'sequelize'
+// import Sequelize from 'sequelize'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const env = process.env.NODE_ENV || 'development'
 const isDev = env === 'development'
-const sequelize = new Sequelize(process.env.DB_URL)
+// const sequelize = new Sequelize(process.env.DB_URL)
 
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connected succesfully!')
-  })
-  .catch(err => {
-    console.log('Unable to connect to the database', err)
-  })
+// sequelize.authenticate()
+//   .then(() => {
+//     console.log('Connected succesfully!')
+//   })
+//   .catch(err => {
+//     console.log('Unable to connect to the database', err)
+//   })
 
 const app = express()
 
